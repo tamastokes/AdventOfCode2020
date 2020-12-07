107e61f9d7975357b2bcd957e111fe4bed46a812
Tamás Tőkés <tamastokes@gmail.com>
2020-12-07 18:34:39 +0100

    Generalize for any number of tags

--- day1.R
+++ day1.R
@@ -24,6 +24,33 @@ mul.summing.to <- function(l, s){
   }
 }
 
+quiz.day1 <- function(l, s){
+  result <- c()
+  d <- length(l)
+  n <- length(l[[1]])
+  t <- n^d
+  for (i in 1:(t)){
+    result <- c(result, mul.summing.to(l, s))
+    # rotate the vectors (similarly to n-base counting)
+    if (i<t){
+      # calculate how many vectors we need to rotate at this step
+      m <- i
+      cnt <- 0
+      while (m %% n == 0){
+        m <- m / n
+        cnt <- cnt + 1
+      }
+      # execute the rotation for the necessary vectors
+      for (j in (d-cnt):d){
+        l[[j]] <- shift.left(l[[j]],1)
+      }
+    }
+  }
+  # double-counting typiccally occurs when a vector is
+  # added to itself shifted by length(v)/2
+  unique(result)
+}
+
 quiz.day1.1 <- function(v, s){
   result <- c()
   for (i in 1:length(v)){
@@ -37,14 +64,18 @@ quiz.day1.1 <- function(v, s){
 quiz.day1.1.test <- function(){
   v <- c(1721, 979, 366, 299, 675, 1456)
   s <- 2020
-  quiz.day1.1(v, s)
+  l <- list(v,v)
+  print(quiz.day1.1(v, s))
+  print(quiz.day1(l, s))
 }
 
 quiz.day1.1.input <- function(){
   t <- read.table("./day1_input")
   v <- t$V1
+  l <- list(v, v)
   s <- 2020
-  quiz.day1.1(v, s)
+  print(quiz.day1.1(v, s))
+  print(quiz.day1(l, s))
 }
 
 quiz.day1.2 <- function(v, s){
@@ -61,12 +92,16 @@ quiz.day1.2 <- function(v, s){
 quiz.day1.2.test <- function(){
   v <- c(1721, 979, 366, 299, 675, 1456)
   s <- 2020
-  quiz.day1.2(v, s)
+  l <- list(v,v,v)
+  print(quiz.day1.2(v, s))
+  print(quiz.day1(l, s))
 }
 
 quiz.day1.2.input <- function(){
   t <- read.table("./day1_input")
   v <- t$V1
+  l <- list(v,v,v)
   s <- 2020
-  quiz.day1.2(v, s)
+  print(quiz.day1.2(v, s))
+  print(quiz.day1(l, s))
 }
