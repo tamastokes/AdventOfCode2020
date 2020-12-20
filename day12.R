70455a8d13a730b37ef181797c00fb25ad2a43a2
Tamás Tőkés <tamastokes@gmail.com>
2020-12-20 20:55:09 +0100

    Solve part 2 of day 12

--- day12.R
+++ day12.R
@@ -5,12 +5,21 @@ turn <- function(curr.direction, left.or.right, degree){
   v[pos]
 }
 
-day12 <- function(filename){
+rotate.waypoint <- function(curr.position, left.or.right, degree){
+  coord.to.negate <- ifelse(left.or.right=='L', 1, 2)
+  for (i in 1:(degree/90)){
+    curr.position <- rev(curr.position)
+    curr.position[coord.to.negate] <- (-1)*curr.position[coord.to.negate]
+  }
+  curr.position
+}
+
+day12 <- function(filename, part=1){
   f <- read.table(filename, as.is = TRUE)
-  directions <- c('N','E','S','W')
-  instructions <- NULL
-  quantities <- NULL
-  direction <- 'E'
+  directions <- c('E','N','S','W')
+  instructions <- directions
+  quantities <- c(10, 1, 0, 0) # initial values coming from the problem-definition
+  spaceship.pos <- c(0,0)
   for (e in f$V1){
     instr <- substr(e,1,1)
     val <- as.integer(substr(e,2,nchar(e)))
@@ -20,16 +29,27 @@ day12 <- function(filename){
     }
     else{
       if (instr=='F'){
-        instructions <- c(instructions, direction)
-        quantities <- c(quantities, val)
+        groupped <- tapply(quantities,instructions,sum)
+        total.north <- groupped['N']-groupped['S']
+        total.east <- groupped['E']-groupped['W']
+        spaceship.pos <- spaceship.pos + val*c(total.north,total.east)
+        cat("F, pos: ", spaceship.pos, "\n")
+        print(groupped)
       }
       if (any(instr=='R',instr=='L')){
-        direction <- turn(direction, instr, val)
-        instructions <- c(instructions, direction)
-        quantities <- c(quantities, 0)
+        groupped <- tapply(quantities,instructions,sum)
+        total.north <- groupped['N']-groupped['S']
+        total.east <- groupped['E']-groupped['W']
+        pos <- c(total.east, total.north)
+        cat("RL pos: ", pos, "\n")
+        new.position <- rotate.waypoint(pos,instr,val)
+        cat(" new pos: ", new.position, "\n")
+        instructions <- directions
+        quantities <- c(new.position, 0, 0)
+        cat(" new instr: ", instructions, "\n")
+        cat(" new quant: ", quantities, "\n")
       }
     }
   }
-  groupped <- tapply(quantities,instructions,sum)
-  abs(groupped['N']-groupped['S']) + abs(groupped['E']-groupped['W'])
+  sum(abs(spaceship.pos))
 }
