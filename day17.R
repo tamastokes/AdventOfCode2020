7f1bcb299edaffdfe12c74e49fa700c56640eb00
Tamás Tőkés <tamastokes@gmail.com>
2020-12-26 13:55:57 +0100

    Solve part 1 of day 17

---
+++ day17.R
@@ -0,0 +1,107 @@
+
+print.a <- function(a)
+{
+  nrows <- dim(a)[1]
+  ncols <- dim(a)[2]
+  nheight <- dim(a)[3]
+  for (h in 1:nheight){
+    for (i in 1:nrows){
+      for (j in 1:ncols){
+        if (a[i,j,h] == 1){
+          cat('#')
+        }
+        if (a[i,j,h] == 0){
+          cat('.')
+        }
+      }
+      cat('\n')
+    }
+    cat('\n')
+  }
+  cat('\n')
+}
+
+expand.matrix <- function(m){
+  nrows <- dim(m)[1]
+  ncols <- dim(m)[2]
+  m <- cbind(rep(0,nrows),m)
+  m <- cbind(m,rep(0,nrows))
+  m <- rbind(rep(0,ncols+2),m)
+  m <- rbind(m,rep(0,ncols+2))
+  m
+}
+
+expand.cube <- function(a){
+  dims <- dim(a)+2
+  v <- rep(0,dims[1]*dims[2])
+  for (h in 1:(dim(a)[3])){
+    v <- c(v, expand.matrix(a[,,h]))
+  }
+  v <- c(v, rep(0,dims[1]*dims[2]))
+  dim(v) <- dims
+  v
+}
+
+calc.activations <- function(a){
+  nx <- dim(a)[1]
+  ny <- dim(a)[2]
+  nz <- dim(a)[3]
+  b <- array(0, dim(a))
+  for (z in 2:(nz-1)){
+    for (x in 2:(nx-1)){
+      for (y in 2:(ny-1)){
+        neighbours <- a[(x-1):(x+1),(y-1):(y+1),(z-1):(z+1)]
+        #cat(x,y,z, " ", a[x,y,z], " ", sum(neighbours),  "\n")
+        #print(array(neighbours,c(3,3,3)))
+        if (a[x,y,z] == 1){
+          if (sum(neighbours) == 3 | sum(neighbours) == 4){ # sum contains the node itself!
+            b[x,y,z] = 1
+          }
+          else{
+            b[x,y,z] = 0
+          }
+        }
+        else {
+          if (sum(neighbours) == 3){
+            b[x,y,z] = 1
+          }
+          else{
+            b[x,y,z] = 0
+          }
+        }
+      }
+    }
+  }
+  b
+}
+
+day17 <- function(filename){
+  f <- read.table(filename, as.is=TRUE, comment.char = "")
+  nrows <- length(f$V1)
+  ncols <- nchar(f$V1[1])
+  a <- rep(0, nrows*ncols)
+  dim(a) <- c(nrows,ncols,1)
+  for (i in seq(nrows)){
+    for (j in seq(ncols))
+    {
+      ch <- substr(f$V1[i],j,j)
+      if(ch=="#") a[i,j,1] <- 1
+    }
+  }
+  a <- expand.cube(a)
+  a <- expand.cube(a)
+  print.a(a)
+  a <- calc.activations(a)
+  a <- expand.cube(a)
+  a <- calc.activations(a)
+  a <- expand.cube(a)
+  a <- calc.activations(a)
+  a <- expand.cube(a)
+  a <- calc.activations(a)
+  a <- expand.cube(a)
+  a <- calc.activations(a)
+  a <- expand.cube(a)
+  a <- calc.activations(a)
+  print.a(a)
+  sum(a)
+}
